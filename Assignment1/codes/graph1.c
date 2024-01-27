23988583bc47ca545579f61042738d41b774c54d
root <root@localhost.localdomain>
2024-01-27 15:52:50 +0000

    first commit

--- Assignment1/codes/graph1.c
+++ Assignment1/codes/graph1.c
@@ -5,11 +5,21 @@ int main() {
         printf("Error opening file!\n");
         return 1;
     }
-    int i;
-    fprintf(file,"n \t First \t Second\n");
-    for(i=0;i<51;i++)
+    float first_term_1 = 7;
+    float first_term_2 = 18;
+
+    float common_difference_1 = 6;     //13 - 7
+    float common_difference_2 = -2.5;   //15.5 - 18
+
+    int n ; // nth term of the series 
+    float x1;  //term of the first series
+    float x2;  //term of the second series
+
+    for(n=0;n<51;n++)
     {
-        fprintf(file ,"%d \t %f \t %f",i , 7 + 6*i, 18 - 2.5*i)  ;
+        x1 = first_term_1 + (float)(n)*(common_difference_1);
+        x2 = first_term_2 + (float)(n)*(common_difference_2);
+        fprintf(file ,"%d \t %e \t %e\n",n , x1 , x2);
     }
     fclose(file);
     return 0;
