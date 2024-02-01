89d86cbe6f99d8677b74c1b6b87f1327136c0345
root <root@localhost.localdomain>
2024-02-01 05:22:05 +0000

    first commit

--- Assignment2/codes/graph1.py
+++ Assignment2/codes/graph1.py
@@ -8,14 +8,18 @@ data_array = np.array(list(map(np.vectorize(float), map(lambda line: line.split(
 x = data_array[:, 0].astype(int)
 y1 = data_array[: , 1]
 y2 = data_array[:, 2]
-plt.xlim(0,20)
+plt.xlim(0, 20)
 plt.ylim(0, 2500)
-plt.stem(x, y2,markerfmt='o')
-#plt.title("Plotting x(n)")
+plt.stem(x, y2, markerfmt='o', linefmt='b-', basefmt='b-') 
+plt.scatter(x, y2, color='blue')  
 plt.xlabel("n")
-plt.ylabel("s(n)")
-plt.xticks(np.arange(0,21, 1))
-plt.yticks(np.arange(0,2500, 100))
+plt.ylabel("y(n)")
+plt.xticks(np.arange(0, 21, 1))
+plt.yticks(np.arange(0, 2500, 100))
+
+index_y6 = np.where(x == 6)[0][0]
+plt.stem(x[index_y6], y2[index_y6], markerfmt='o', linefmt='r-', basefmt='r-')
+
 #plt.grid(True)
 plt.savefig("Figure_1.png")
 plt.show()
