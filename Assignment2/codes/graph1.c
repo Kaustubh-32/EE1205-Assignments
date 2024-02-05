75918cfa7d8a311e43a2f311c0b9873305e36fed
root <root@localhost.localdomain>
2024-02-05 09:49:44 +0000

    update

--- Assignment2/codes/graph1.c
+++ Assignment2/codes/graph1.c
@@ -2,12 +2,12 @@
 #include<math.h>
 float y(int n); //recursive function to calculate the sum of GP : y(n) = sum of first (n+1) terms of the GP 
 
-float first_term = 729;    //first term of the GP
+float a = 729;    //first term of the GP
 
-float common_ratio = 2.0 / 3.0 ;  //common ratio of thr GP
+float r = 2.0 / 3.0 ;  //common ratio of thr GP
 
 int main() {
-    FILE *file = fopen("values.dat", "w");
+    FILE *file = fopen("values.dat", "w"); //open values.dat to print the data in it
     if (file == NULL) {
         printf("Error opening file!\n");
         return 1;
@@ -18,16 +18,16 @@ int main() {
     
     for(n=0;n<=21;n++)
     {
-        x = first_term*(pow(common_ratio,n));
-        fprintf(file ,"%d \t %e \t %e\n",n , x , y(n));
+        x = a*(pow(r,n));
+        fprintf(file ,"%d \t %e \t %e\n",n , x , y(n)); //print n,x(n), y(n)
     }
-    fclose(file);
+    fclose(file);     //close the file after printing the output
     return 0;
 }
 float y(int n)
 {
     if(n==0)
-        return first_term;
+        return a; //return first term for y(0)
     else
-        return y(n-1) + (float)pow(common_ratio,n)*(first_term);  //sum of n terms  = sum of n-1 terms + nth term
+        return y(n-1) + (float)pow(r,n)*(a);  //sum of n terms  = sum of n-1 terms + nth term
 }
